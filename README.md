77755f58dc2cf48d01a3ed967309e48b85ae0ff7
tbjgolden <tbjgolden@gmail.com>
2018-02-04 17:11:03 -0800

    Release v0.6 with js api

--- README.md
+++ README.md
@@ -41,19 +41,54 @@
     colors: ['gray', '#090', '#c00', 'purple', '#00c', '#ccc', '#333', 'yellow', 'rgb(240,240,240)']
   };
 
-  createJSONViewer(container, json, options);
+  var viewer = createJSONViewer(container, json, options);
+
+  viewer.changeJSON({ youCanAlsoChangeTheJSON: 'after the viewer is created' });
 </script>
 ```
 
+### `methods`
+
+- `JSONViewer.changeJSON(json<String|Object>, path<Array> {optional, default: []})`  
+  ```javascript
+  var viewer = createJSONViewer(el, json, opts);
+
+  setTimeout(function () {
+    // without path, replaces all of the json
+    viewer.changeJSON({ "array": [ { "i-have-been-modified": false } ] });
+  }, 1000);
+
+  setTimeout(function () {
+    // with path, replaces specific part of the json
+    viewer.changeJSON(true, ["array", 0, "i-have-been-modified"]);
+  }, 2000);
+  ```
+
 ### `options`
 
-Currently only two:
-
-- `fontFamily` (default `'monospace'`)  
+- `fontFamily` {default: `'monospace'`}  
   If you want to use a custom font (you'll have to load it yourself).
-- `colors` (default `['#999', '#090', '#c00', '#c0c', '#00c', '#ccc', '#333', '#ff0', '#eee']`)  
+- `colors` {default: `['#999', '#090', '#c00', '#c0c', '#00c', '#ccc', '#333', '#ff0', '#eee']`}  
   An array of 9 CSS colors as strings. To see what each one does, look **[here]()**
   (numbers before `vmax` corresponds to the index, so `color: 3vmax` means `color: #c0c` with defaults)
+- `onValueClick` {default: none}  
+  onValueClick allows you to pass a handler when a value is clicked. It will be the closest
+  ancestor that is either a key-value pair from an object, or an item from an array.
+  ```javascript
+  var json = {
+    "someArray": [
+      "clickMe"
+    ]
+  };
+
+  var viewer = createJSONViewer(el, json, {
+    onValueClick: function (valueOfClicked, pathToClicked, element) {
+      console.log(valueOfClicked); // "clickMe"
+      console.log(pathToClicked);  // ["someArray", 0]
+      console.log(element);        // div.json-viewer-array-value
+    }
+  });
+  ```
 
 ### Finally
 
--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@
       this.searchValue = '';
       this.json = json;
       this.opts = opts;
+      this.values = [];
 
       this.container.appendChild(search);
       this.container.appendChild(jsonViewer);
@@ -47,47 +48,79 @@
       this.uid = `time-${Date.now()}`;
       el.classList.add(this.uid);
 
+      addCustomStyles(this.uid, this.opts);
       this.generate();
     }
 
     generate () {
-      addCustomStyles(this.uid, this.opts);
-      const html = htmlify(JSON.parse(this.json)) || '';
+      this.values = [];
+
+      const html = this.htmlify(JSON.parse(this.json)) || '';
       this.el.innerHTML = html;
-      this.el.querySelectorAll('.json-viewer-toggle').forEach(toggle => {
-        toggle.addEventListener('click', e => {
-          e.target.parentNode.classList.toggle('json-viewer-closed');
-        });
+
+      this.el.querySelectorAll('[data-json-id]').forEach(elWithValue => {
+        elWithValue._value = this.values[elWithValue.getAttribute('data-json-id')];
       });
+
+      this.attachListeners();
+
       this.allNodes = allDescendents(this.el);
     }
 
-    searchInputKeyUp () {
-      const newValue = this.searchEl.value;
-      const prevValue = this.searchValue;
-      if (newValue !== prevValue) {
-        if (newValue) {
-          this.el.classList.add('json-viewer-searching');
-          this.search(newValue);
-        } else {
-          this.el.classList.remove('json-viewer-searching');
-          this.allNodes.forEach(n => {
-            if (n.nodeType === 1) {
-              return (
-                n.classList.contains('json-viewer-collection') &&
-                n.classList.remove('json-viewer-closed')
-              );
-            }
-            n.parentNode.classList.remove('json-viewer-match');
-          });
-        }
+    getJSON () {
+      return JSON.parse(this.json);
+    }
+
+    changeJSON (json = {}, path = []) {
+      if (typeof json === 'string') json = JSON.parse(json);
+
+      const prevJSON = this.getJSON();
+      let fragmentToMutate = prevJSON;
+
+      while (path.length > 1) {
+        fragmentToMutate = fragmentToMutate[path.shift()];
       }
-      this.searchValue = newValue;
+
+      if (path.length) {
+        fragmentToMutate[path[0]] = json;
+        this.json = JSON.stringify(prevJSON);
+      } else {
+        this.json = JSON.stringify(json);
+      }
+
+      this.generate();
+      this.searchValue = null;
+      this.searchInputKeyUp();
+    }
+
+    searchInputKeyUp () {
+      clearTimeout(this.debounce);
+
+      this.debounce = setTimeout(() => {
+        const newValue = this.searchEl.value;
+        const prevValue = this.searchValue;
+        if (newValue !== prevValue) {
+          if (newValue) {
+            this.el.classList.add('json-viewer-searching');
+            this.search(newValue);
+          } else {
+            this.el.classList.remove('json-viewer-searching');
+            this.allNodes.forEach(n => {
+              if (n.nodeType === 1) {
+                return (
+                  n.classList.contains('json-viewer-collection') &&
+                  n.classList.remove('json-viewer-closed')
+                );
+              }
+              n.parentNode.classList.remove('json-viewer-match');
+            });
+          }
+        }
+        this.searchValue = newValue;
+      }, 200);
     }
 
     search (query) {
-      clearTimeout(this.debounce);
-
       let regex = null;
       try {
         if (!/\/[^/]*\/[\s\S]*/.test(query)) throw new Error();
@@ -100,27 +133,126 @@
       }
       this.searchEl.style.color = regex ? this.opts.colors[4] : this.opts.colors[6];
 
-      this.debounce = setTimeout(() => {
-        this.allNodes.forEach(n => {
-          if (n.nodeType === 1) {
-            return (
-              n.classList.contains('json-viewer-collection') &&
-              n.classList.add('json-viewer-closed')
+      this.allNodes.forEach(n => {
+        if (n.nodeType === 1) {
+          return (
+            n.classList.contains('json-viewer-collection') &&
+            n.classList.add('json-viewer-closed')
+          );
+        }
+
+        if (isMatch(regex || query, n.textContent)) {
+          n.parentNode.classList.add('json-viewer-match');
+          let curr = getContainingCollectionEl(n);
+          while (curr) {
+            curr.classList.remove('json-viewer-closed');
+            curr = getContainingCollectionEl(curr);
+          }
+        } else {
+          n.parentNode.classList.remove('json-viewer-match');
+        }
+      });
+    }
+
+    htmlify (variable, path = []) {
+      const type = getType(variable);
+
+      switch (type) {
+        case 'number': case 'boolean':
+          return '<span class="json-viewer-' + type + '">' + variable + '</span>';
+        case 'string':
+          return (
+            '<span class="json-viewer-string">' +
+              escapeString(variable) +
+            '</span>'
+          );
+        case 'array':
+          return (
+            '<span class="json-viewer-array json-viewer-collection">' +
+              '<button class="json-viewer-toggle"></button>' +
+              '<span class="json-viewer-array-contents">' +
+                variable.map((variable, index) => {
+                  this.values.push(variable);
+                  return (
+                    '<span class="json-viewer-array-value" data-index="' + index +
+                    '" data-json-id="' + (this.values.length - 1) +
+                    '" data-location="' + encodeURIComponent(JSON.stringify(path)) + '">' +
+                      this.htmlify(variable, [...path, index]) +
+                    '</span>'
+                  );
+                }).join('') +
+              '</span>' +
+            '</span>'
+          );
+        case 'object':
+          return (
+            '<span class="json-viewer-object json-viewer-collection">' +
+              '<button class="json-viewer-toggle"></button>' +
+              '<span class="json-viewer-object-contents">' +
+                Object.keys(variable).map(key => {
+                  this.values.push(key);
+                  this.values.push(variable[key]);
+                  return (
+                    '<span class="json-viewer-key-value-pair" data-location="' + encodeURIComponent(JSON.stringify(path)) + '">' +
+                      '<span class="json-viewer-object-key" data-json-id="' + (this.values.length - 2) + '">' +
+                        escapeString(key) +
+                      '</span>' +
+                      '<span class="json-viewer-object-value" data-json-id="' + (this.values.length - 1) + '">' +
+                        this.htmlify(variable[key], [...path, key]) +
+                      '</span>' +
+                    '</span>'
+                  );
+                }).join('') +
+              '</span>' +
+            '</span>'
+          );
+      }
+    }
+
+    attachListeners () {
+      this.el.querySelectorAll('.json-viewer-toggle').forEach(toggle => {
+        toggle.addEventListener('click', e => {
+          e.target.parentNode.classList.toggle('json-viewer-closed');
+        });
+      });
+      this.el.querySelectorAll('.json-viewer-key-value-pair').forEach(kvPair => {
+        kvPair.addEventListener('click', e => {
+          e.stopPropagation();
+
+          if (this.opts.onValueClick) {
+            let el = e.target;
+            while (!el.classList.contains('json-viewer-key-value-pair')) el = el.parentNode;
+
+            this.opts.onValueClick(
+              el.querySelector('.json-viewer-object-value')._value,
+              [
+                ...JSON.parse(decodeURIComponent(el.getAttribute('data-location'))),
+                el.querySelector('.json-viewer-object-key')._value
+              ],
+              el
             );
           }
+        });
+      });
+      this.el.querySelectorAll('.json-viewer-array-value').forEach(arrValue => {
+        arrValue.addEventListener('click', e => {
+          e.stopPropagation();
 
-          if (isMatch(regex || query, n.textContent)) {
-            n.parentNode.classList.add('json-viewer-match');
-            let curr = getContainingCollectionEl(n);
-            while (curr) {
-              curr.classList.remove('json-viewer-closed');
-              curr = getContainingCollectionEl(curr);
-            }
-          } else {
-            n.parentNode.classList.remove('json-viewer-match');
+          if (this.opts.onValueClick) {
+            let el = e.target;
+            while (!el.classList.contains('json-viewer-array-value')) el = el.parentNode;
+
+            this.opts.onValueClick(
+              el._value,
+              [
+                ...JSON.parse(decodeURIComponent(el.getAttribute('data-location'))),
+                ~~el.getAttribute('data-index')
+              ],
+              el
+            );
           }
         });
-      }, 150);
+      });
     }
   }
 
@@ -156,49 +288,6 @@
       if (si === string.length) return false;
       if (query.charCodeAt(qi) === string.charCodeAt(si)) qi++;
       si++;
-    }
-  }
-
-  function htmlify (variable) {
-    const type = getType(variable);
-    switch (type) {
-      case 'number': case 'boolean':
-        return '<span class="json-viewer-' + type + '">' + variable + '</span>';
-      case 'string':
-        return (
-          '<span class="json-viewer-string">' +
-            escapeString(variable) +
-          '</span>'
-        );
-      case 'array':
-        return (
-          '<span class="json-viewer-array json-viewer-collection">' +
-            '<button class="json-viewer-toggle"></button>' +
-            '<span class="json-viewer-array-contents">' +
-              variable.map(variable => (
-                '<span class="json-viewer-array-value">' + htmlify(variable) + '</span>'
-              )).join('') +
-            '</span>' +
-          '</span>'
-        );
-      case 'object':
-        return (
-          '<span class="json-viewer-object json-viewer-collection">' +
-            '<button class="json-viewer-toggle"></button>' +
-            '<span class="json-viewer-object-contents">' +
-              Object.keys(variable).map(key =>
-                '<span class="json-viewer-key-value-pair">' +
-                  '<span class="json-viewer-object-key">' +
-                    escapeString(key) +
-                  '</span>' +
-                  '<span class="json-viewer-object-value">' +
-                    htmlify(variable[key]) +
-                  '</span>' +
-                '</span>'
-              ).join('') +
-            '</span>' +
-          '</span>'
-        );
     }
   }
 
